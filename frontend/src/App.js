4fd26377cd6114b3ae68b52fe840fcdb33f8646c
Shravani Parsi <shravaniparsi@Shravanis-MacBook-Pro.local>
2022-04-05 15:28:07 -0700

    Add App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./containers/Navbar/Navbar";
+import Homepage from "./components/Homepage/Homepage";
+import SideBar from "./containers/Sidebar/Sidebar";
+import Tags from "./components/Tags/Tags";
+import Users from "./components/Users/Users";
+// import Footer from './containers/Footer/Footer';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <main className="d-flex">
+        <SideBar />
+        <div className="content container-fluid mt-4 ml-3 p-0">
+          <Routes>
+            <Route path="/" exact element={<Homepage />} />
+            <Route path="/tags" exact element={<Tags />} />
+            <Route path="/users" exact element={<Users />} />
+          </Routes>
+        </div>
+      </main>
+      {/* <Footer /> */}
+    </BrowserRouter>
+  );
+}
+
+export default App;
