1753a0d7dc6f9124f0eb516e469d1c5116f674cd
Shravani Parsi <shravaniparsi@Shravanis-MacBook-Pro.local>
2022-04-05 15:33:41 -0700

    sidebar implementation initial

---
+++ frontend/src/containers/Sidebar/Sidebar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <>
+      <div className="sidebar-wrapper">
+        <div className="sidebar-container">
+          <ul className="sidebar-navigation pt-4">
+            <li>
+              <a href="/" className="side-bar-menu-item">
+                Home
+              </a>
+            </li>
+          </ul>
+
+          <ul className="sidebar-navigation pt-4">
+            <li class="side-bar-menu-item sidebar-heading">PUBLIC</li>
+            <li>
+              <a href="/tags" className="side-bar-menu-item">
+                Tags
+              </a>
+            </li>
+            <li>
+              <a href="/users" className="side-bar-menu-item">
+                Users
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Sidebar;
