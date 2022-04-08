e7ceebf056a5d26cd3bf4b41e097c64f4b624596
Shravani Parsi <shravaniparsi@Shravanis-MacBook-Pro.local>
2022-04-08 16:06:52 -0700

    modals initialised

--- backend/models/questionModel.js
+++ backend/models/questionModel.js
@@ -1,14 +1,23 @@
 const mongoose = require("mongoose");
+const answerSchema = require("./answerModel");
+const commentSchema = require("./commentModel");
+const tagSchema = require("./TagModel");
 
 const questionSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    tags: { type: String, required: true },
     questionbody: { type: String, required: true },
-    userToken: { type: String, required: true },
+    userId: { type: mongoose.Types.ObjectId, required: true },
+    votes: { type: Number, required: false },
+    views: { type: Number, required: false },
+    tags: [tagSchema.schema],
+    answers: [answerSchema.schema],
+    comments: [commentSchema.schema],
+    waitingForApproval: { type: Boolean, required: false },
   },
   {
     versionKey: false,
+    timestamps: true,
   }
 );
 
