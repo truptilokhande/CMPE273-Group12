751fac2e872fcadac7257f47cce227c550b692e9
Shravani Parsi <shravaniparsi@Shravanis-MacBook-Pro.local>
2022-04-08 10:03:14 -0700

    adding new page routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,24 +7,39 @@ import SideBar from "./containers/Sidebar/Sidebar";
 import Tags from "./components/Tags/Tags";
 import Users from "./components/Users/Users";
 import AskQuestion from "./components/AskQuestion/AskQuestion";
+import QuestionOverview from "./components/QuestionOverview/QuestionOverview";
+import TagOverview from "./components/TagOverview/TagOverview";
+import SearchPage from "./components/SearchPage/SearchPage";
+import SignUp from "./components/SignUp/SignUp";
+import SignIn from "./components/SignIn/SignIn";
 // import Footer from './containers/Footer/Footer';
 
 function App() {
   return (
     <BrowserRouter>
-        <Navbar />
-        <main className="d-flex">
-          <SideBar />
-          <div className="content container-fluid mt-4 ml-3 p-0">
-            <Routes>
-              <Route path="/" exact element={<Homepage />} />
-              <Route path="/tags" exact element={<Tags />} />
-              <Route path="/users" exact element={<Users />} />
-              <Route path="/askQuestion" exact element={<AskQuestion />} />
-            </Routes>
-          </div>
-        </main>
-        {/* <Footer /> */}
+      <Navbar />
+      <main className="d-flex">
+        <SideBar />
+
+        <div className="content container-fluid mt-4 ml-3 p-0 mb-4">
+          <Routes>
+            <Route path="/" exact element={<Homepage />} />
+            <Route path="/tags" exact element={<Tags />} />
+            <Route path="/users" exact element={<Users />} />
+            <Route path="/askQuestion" exact element={<AskQuestion />} />
+            <Route
+              path="/questionOverview"
+              exact
+              element={<QuestionOverview />}
+            />
+            <Route path="/tagOverview" exact element={<TagOverview />} />
+            <Route path="/search" exact element={<SearchPage />} />
+            <Route path="/signup" exact element={<SignUp />} />
+            <Route path="/signin" exact element={<SignIn />} />
+          </Routes>
+        </div>
+      </main>
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
