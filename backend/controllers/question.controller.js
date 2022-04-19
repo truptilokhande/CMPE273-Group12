e5c7f0f7489f9dc84c60248d8adb8c298a1141f3
shravya-k <k.shravya96@gmail.com>
2022-04-19 10:53:23 -0700

    question api changes

--- backend/controllers/question.controller.js
+++ backend/controllers/question.controller.js
@@ -1,14 +1,12 @@
 const Question = require("../models/question.model");
 
 const addquestion = async (req, res) => {
-  userToken = req.body.userToken;
-  title = req.body.title;
-  tags = req.body.tags;
-  questionbody = req.body.questionbody;
+  const userToken = req.body.userToken;
+  const title = req.body.title;
+  const tags = req.body.tags;
+  const questionbody = req.body.questionbody;
   
-  console.log(userToken, title, tags, questionbody);
   const new_question = {userId:userToken,title: title, tags: tags, questionbody: questionbody, views:0,waitingForApproval:false,answers:[],comments:[],votes:0 }
-  console.log(new_question)
   new Question(new_question).save()
   .then((result) => {
     res.send({
@@ -28,10 +26,10 @@ const addquestion = async (req, res) => {
 }
 
 const editquestion = async (req, res) => {
-  questionId =req.body.questionId
-  title = req.body.title;
-  tags = req.body.tags;
-  questionbody = req.body.questionbody;
+  const questionId =req.body.questionId
+  const title = req.body.title;
+  const tags = req.body.tags;
+  const questionbody = req.body.questionbody;
   Question.updateOne({_id:questionId},{ $set:{title: title, tags: tags, questionbody: questionbody}}, function(err, res) {
     if (err) throw err;
     console.log("1 document updated");
@@ -40,29 +38,22 @@ const editquestion = async (req, res) => {
 }
 
 const getquestion = async (req, res) => {
-  questionId =req.params.questionId
+  const questionId =req.params.questionId
   const currtimestamp = new Date()
   const todaysdate = currtimestamp.toLocaleString("en-US", {timeZone:
     "America/Los_Angeles"}).slice(0,9);
   const datenow_arr = todaysdate.split("/")
-  console.log(datenow_arr)
   Question.updateOne({_id:questionId},{ $inc: { views:1 } }, function(err, result) {
     if (err) throw err;
-    console.log("1 document updated");
     Question.findOne({_id:questionId}, function(err, resultafterviews) {
       if (err) throw err;
       const answerscount = resultafterviews.answers.length
       const createdAt = resultafterviews.createdAt.toLocaleString("en-US", {timeZone:
         "America/Los_Angeles"}).slice(0,9);
       const creationdate_arr = createdAt.split("/")
-      console.log(creationdate_arr)
-      console.log(todaysdate,createdAt)
-      
-      
       const monthdiff = parseInt(datenow_arr[0]) - parseInt(creationdate_arr[0])
       const daysdiff = parseInt(datenow_arr[1]) - parseInt(creationdate_arr[1])
       const yeardiff = parseInt(datenow_arr[2]) - parseInt(creationdate_arr[2])
-      console.log(monthdiff,daysdiff,yeardiff)
       res.send({resultafterviews:resultafterviews,answerscount:answerscount,monthdiff:monthdiff,daysdiff:daysdiff,yeardiff:yeardiff})
     })
   });
