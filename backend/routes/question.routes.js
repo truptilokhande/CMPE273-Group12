363bca660096a5e273cddd2f4fbb85180394f3f5
Shravani Parsi <shravaniparsi@Shravanis-MacBook-Pro.local>
2022-04-21 21:42:12 -0700

    questions by search apis routes added

--- backend/routes/question.routes.js
+++ backend/routes/question.routes.js
@@ -6,8 +6,11 @@ const { authenticateUser } = require("../middleware/authMiddleware");
 router.get("/getQuestions", questionController.getQuestions);
 router.post("/addquestion", questionController.addquestion);
 router.get("/getquestion/:questionId", questionController.getquestion);
+router.get("/searchQuestionsByUserId/:searchkey", questionController.searchQuestionsByUserId);
+router.get("/searchQuestionsByText/:searchkey", questionController.searchQuestionsByText);
 router.post("/voteQuestion", questionController.voteQuestion);
 router.post("/editquestion", questionController.editquestion);
 router.post("/bookmark", questionController.bookmarkQuestion);
+
 
 module.exports = router;
