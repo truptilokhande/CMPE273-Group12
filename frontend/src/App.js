9d36f7a1626220d267c29dc0f77574d4c6647176
Vinay Kumar Reddy <vinayseelam.98@gmail.com>
2022-04-26 15:49:39 -0700

    Adding Timeline Endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,12 +17,13 @@ import ProfilePage from "./components/Profile/ProfilePage";
 import Activities from "./components/Profile/Activities/Activities";
 import Questionstab from "./components/Profile/Activities/Questions/Questionstab";
 import UserTags from "./components/Profile/Activities/UserTags/UserTags";
-import ActivityBadges from './components/Profile/Activities/ActivityBadges/ActivityBadges'
+import ActivityBadges from "./components/Profile/Activities/ActivityBadges/ActivityBadges";
 import Bookmarkstab from "./components/Profile/Activities/Bookmarkstab/Bookmarkstab";
 import Reputation from "./components/Profile/Activities/Reputation/Reputation";
 import BasicDetails from "./components/Profile/BasicDetails/BasicDetails";
 import Addtag from "./components/Admin/Addtag";
 import Aproove from "./components/Admin/Aproove";
+import TimeLine from "./components/QuestionOverview/TimeLine";
 // import Footer from './containers/Footer/Footer';
 // import Footer from './containers/Footer/Footer';
 
@@ -48,16 +49,33 @@ function App() {
             <Route path="/signup" exact element={<SignUp />} />
             <Route path="/signin" exact element={<SignIn />} />
             <Route path="/edit-question" exact element={<EditQuestion />} />
-            <Route path="/userProfile" exact element={<ProfilePage/>}/>
-              <Route path="/Activities" exact element={<Activities/>}/>
-              <Route path="/Questions/Questionstab" exact element={<Questionstab/>}/>
-              <Route path="/UserTags/UserTags" exact element={<UserTags/>}/>
-              <Route path="/ActivityBadges/ActivityBadges" exact element={<ActivityBadges/>}/>
-              <Route path="/Bookmarkstab/Bookmarkstab" exact element={<Bookmarkstab/>}/>
-              <Route path="/Reputation/Reputation" exact element={<Reputation/>}/>
-              <Route path="/BasicDetails" exact element={<BasicDetails/>}/>
-              <Route path="/Addtag" exact element={<Addtag/>}/>
-              <Route path="/Aproove" exact element={<Aproove/>}/>
+            <Route path="/userProfile" exact element={<ProfilePage />} />
+            <Route path="/Activities" exact element={<Activities />} />
+            <Route
+              path="/Questions/Questionstab"
+              exact
+              element={<Questionstab />}
+            />
+            <Route path="/UserTags/UserTags" exact element={<UserTags />} />
+            <Route
+              path="/ActivityBadges/ActivityBadges"
+              exact
+              element={<ActivityBadges />}
+            />
+            <Route
+              path="/Bookmarkstab/Bookmarkstab"
+              exact
+              element={<Bookmarkstab />}
+            />
+            <Route
+              path="/Reputation/Reputation"
+              exact
+              element={<Reputation />}
+            />
+            <Route path="/BasicDetails" exact element={<BasicDetails />} />
+            <Route path="/Addtag" exact element={<Addtag />} />
+            <Route path="/Aproove" exact element={<Aproove />} />
+            <Route path="/timeline" exact element={<TimeLine />} />
           </Routes>
         </div>
       </main>
