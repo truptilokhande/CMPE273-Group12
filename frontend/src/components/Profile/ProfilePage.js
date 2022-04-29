85c30dbfd53b6f18e8a49204ea0b5b1ef31e87c1
shravya-k <k.shravya96@gmail.com>
2022-04-29 01:39:48 -0700

    added new chat option to profile page

--- frontend/src/components/Profile/ProfilePage.js
+++ frontend/src/components/Profile/ProfilePage.js
@@ -1,6 +1,6 @@
 import './ProfilePage.css';
 
-import React, {useEffect, Fragment} from 'react';
+import React, {useEffect, Fragment,useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from "react-router-dom";
 import goldTag from "../../assets/goldbadge.png";
@@ -8,21 +8,55 @@ import silverTag from "../../assets/silverbadge.png";
 import bronzeTag from "../../assets/bronzebadge.png";
 import Spinner from './Spinner/Spinnercomponent.js';
 
-
 import TopPosts from "./TopPosts/TopPosts.js"
 import Activities from "./Activities/Activities.js"
 import BasicDetails from './BasicDetails/BasicDetails';
+import axios from 'axios';
 const ProfilePage = () => {
+    const [receiver, setReceiverID] = useState("");
   // useEffect(() => {
   //   getProfile(match.params.id);
   //   // eslint-disable-next-line
   // }, [getProfile]);
 
+function startnewchat() {
+alert("1")
+   console.log(receiver)
+   axios.post('http://localhost:3001'+'/api/messages/sendMessage',{
+        senderID: "1",
+        receiverID:receiver,
+        message:"",
+        
+        })
+        .then(res =>{
+          console.log("%%%",res)
+        }).catch(err => {console.log(err)})
+    
 
+  };
   return (
     <div>
+  
 
       <BasicDetails></BasicDetails>
+
+
+
+       
+         <div className="App">
+        
+        <input
+          type="text" data-testid="username" 
+          onChange={(event) => {
+            setReceiverID(event.target.value);
+          }} placeholder="receiverID"
+        ></input>
+        <button onClick={startnewchat}>Start Chat</button>
+        </div>
+            <br></br>
+
+
+
          <div id='mainbar' className='d-flex flex-col user-main-bar pl24 pt24'>
          <div class="grid--item" data-is-here-when="lg" id="stats">
                         <div class="fs-title mb8">Stats</div>
@@ -50,7 +84,7 @@ and questions where they wrote highly-ranked answers)">
                 <div class="fc-medium mt4 sm:mt2">
                     <svg aria-hidden="true" class="svg-icon iconAchievements" width="18" height="18" viewBox="0 0 18 18"><path d="M15 2V1H3v1H0v4c0 1.6 1.4 3 3 3v1c.4 1.5 3 2.6 5 3v2H5s-1 1.5-1 2h10c0-.4-1-2-1-2h-3v-2c2-.4 4.6-1.5 5-3V9c1.6-.2 3-1.4 3-3V2h-3ZM3 7c-.5 0-1-.5-1-1V4h1v3Zm8.4 2.5L9 8 6.6 9.4l1-2.7L5 5h3l1-2.7L10 5h2.8l-2.3 1.8 1 2.7h-.1ZM16 6c0 .5-.5 1-1 1V4h1v2Z"></path></svg>
                 </div>
-                <div class="s-anchors s-anchors__inherit js-rank-badge">        <a href="https://stackexchange.com/leagues/1/year/stackoverflow/2022-01-01/8690857#8690857" target="_blank">top <b>0.01%</b> this year</a>
+                <div class="s-anchors s-anchors__inherit js-rank-badge">        <a href="https://stackexchange.com/leagues/1/year/stackoverflow/2022-01-01/8690857#8690857" target="_blank" rel="noreferrer">top <b>0.01%</b> this year</a>
 </div>
             </div>
 
