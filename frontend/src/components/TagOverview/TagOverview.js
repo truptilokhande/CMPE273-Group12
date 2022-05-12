e6d4ce953f19fb813156c37d9dec54677b0fa57c
Shravani Parsi <shravaniparsi@Shravanis-MacBook-Pro.local>
2022-05-11 18:25:47 -0700

    tag overview filter with user id in url

--- frontend/src/components/TagOverview/TagOverview.js
+++ frontend/src/components/TagOverview/TagOverview.js
@@ -9,19 +9,26 @@ function TagOverview() {
   const [tag, setTag] = useState();
   // const [questionsCopy, setQuestionsCopy] = useState();
   const token = localStorage.getItem("token");
-  
+
   useEffect(() => {
     const url = window.location.pathname;
     const id = url.substring(url.lastIndexOf("/") + 1);
+    const params = new URLSearchParams(document.location.search);
+    const userId = params.get("userId");
     axios
       .get(
         `${connection.connectionURL}/api/tag/getAllQuestionWithSpecificTag/${id}`,
-        { headers: {"Authorization" : `Bearer ${token}`} }
+        { headers: { Authorization: `Bearer ${token}` } }
       )
       .then((response) => {
         setQuestions(response?.data?.questions);
         setTag(response?.data?.tag);
-        // setQuestionsCopy(response?.data?.userDetails);
+        if (userId) {
+          const filteredQuestions = response?.data?.questions.filter(
+            (i) => i.userId === userId
+          );
+          setQuestions(filteredQuestions);
+        }
       })
       .catch((err) => {
         throw err;
@@ -38,12 +45,12 @@ function TagOverview() {
 
       <div className="d-flex align-items-end justify-content-between mb-3">
         <div className=""> {questions?.length} questions</div>
-        <div className="d-flex flex-row filter-btn-wrappers mt-3">
+        {/* <div className="d-flex flex-row filter-btn-wrappers mt-3">
           <div className="filter-btn">Interesting</div>
           <div className="filter-btn">Hot</div>
           <div className="filter-btn">Score</div>
           <div className="filter-btn fliter-btn-last">Unanswered</div>
-        </div>
+        </div> */}
       </div>
 
       <QuestionsWrapper questions={questions} />
