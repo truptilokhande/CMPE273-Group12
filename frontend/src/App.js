ef5749b211ae0ff9a24394a6d490b678a91eb99f
Shravani Parsi <shravaniparsi@Shravanis-MacBook-Pro.local>
2022-05-11 19:02:24 -0700

    adding route guards on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -47,105 +47,202 @@ function App({ isAuthenticated }) {
           <Routes>
             <Route path="/" exact element={<Homepage />} />
             <Route path="/homepage" exact element={<Homepage />} />
-            <Route path="/tags" exact element={<Tags />} />
-            <Route path="/users" exact element={<Users />} />
+            <Route
+              path="/tags"
+              exact
+              element={isAuthenticated ? <Tags /> : <SignIn />}
+            />
+            <Route
+              path="/users"
+              exact
+              element={isAuthenticated ? <Users /> : <SignIn />}
+            />
             <Route
               path="/askQuestion"
               exact
               element={isAuthenticated ? <AskQuestion /> : <SignIn />}
             />
             <Route path="questionOverview">
-              <Route path=":id" element={<QuestionOverview />} />
+              <Route
+                path=":id"
+                element={isAuthenticated ? <QuestionOverview /> : <SignIn />}
+              />
             </Route>
             <Route path="/tagOverview">
-              <Route path=":id" element={<TagOverview />} />
+              <Route
+                path=":id"
+                element={isAuthenticated ? <TagOverview /> : <SignIn />}
+              />
             </Route>
             <Route path="/timeline">
-              <Route path=":id" element={<TimeLine />} />
+              <Route
+                path=":id"
+                element={isAuthenticated ? <TimeLine /> : <SignIn />}
+              />
             </Route>
-            <Route path="/search" exact element={<SearchPage />} />
+            <Route
+              path="/search"
+              exact
+              element={isAuthenticated ? <SearchPage /> : <SignIn />}
+            />
             <Route path="/signup" exact element={<SignUp />} />
             <Route path="/signin" exact element={<SignIn />} />
-            <Route path="/edit-question" exact element={<EditQuestion />} />
-            <Route path="/userProfile" exact element={<ProfilePage />} />
-            <Route path="/Activities" exact element={<Activities />} />
-            <Route
-              path="/Questions/Questionstab"
-              exact
-              element={<Questionstab />}
-            />
-            <Route path="/UserTags/UserTags" exact element={<UserTags />} />
-            <Route
-              path="/ActivityBadges/ActivityBadges"
-              exact
-              element={<ActivityBadges />}
-            />
-            <Route
-              path="/Bookmarkstab/Bookmarkstab"
-              exact
-              element={<Bookmarkstab />}
-            />
-            <Route
-              path="/Reputation/Reputation"
-              exact
-              element={<Reputation />}
-            />
-            <Route path="/BasicDetails" exact element={<BasicDetails />} />
-            <Route path="/Addtag" exact element={<Addtag />} />
-            <Route path="/Aproove" exact element={<Aproove />} />
-            <Route path="/chat" exact element={<Chat />} />
-            <Route path="/allchats" exact element={<AllChats />} />
             <Route
               path="/edit-question"
               exact
               element={isAuthenticated ? <EditQuestion /> : <SignIn />}
             />
-            <Route path="/userProfile/:id" exact element={<ProfilePage />} />
-            <Route path="/Activities/:id" exact element={<Activities />} />
+            <Route
+              path="/userProfile"
+              exact
+              element={isAuthenticated ? <ProfilePage /> : <SignIn />}
+            />
+            <Route
+              path="/Activities"
+              exact
+              element={isAuthenticated ? <Activities /> : <SignIn />}
+            />
+            <Route
+              path="/Questions/Questionstab"
+              exact
+              element={<Questionstab />}
+            />
+            <Route
+              path="/UserTags/UserTags"
+              exact
+              element={isAuthenticated ? <UserTags /> : <SignIn />}
+            />
+            <Route
+              path="/ActivityBadges/ActivityBadges"
+              exact
+              element={isAuthenticated ? <ActivityBadges /> : <SignIn />}
+            />
+            <Route
+              path="/Bookmarkstab/Bookmarkstab"
+              exact
+              element={isAuthenticated ? <Bookmarkstab /> : <SignIn />}
+            />
+            <Route
+              path="/Reputation/Reputation"
+              exact
+              element={isAuthenticated ? <Reputation /> : <SignIn />}
+            />
+            <Route
+              path="/BasicDetails"
+              exact
+              element={isAuthenticated ? <BasicDetails /> : <SignIn />}
+            />
+            <Route
+              path="/Addtag"
+              exact
+              element={isAuthenticated ? <Addtag /> : <SignIn />}
+            />
+            <Route
+              path="/Aproove"
+              exact
+              element={isAuthenticated ? <Aproove /> : <SignIn />}
+            />
+            <Route
+              path="/chat"
+              exact
+              element={isAuthenticated ? <Chat /> : <SignIn />}
+            />
+            <Route
+              path="/allchats"
+              exact
+              element={isAuthenticated ? <AllChats /> : <SignIn />}
+            />
+            <Route
+              path="/edit-question"
+              exact
+              element={isAuthenticated ? <EditQuestion /> : <SignIn />}
+            />
+            <Route
+              path="/userProfile/:id"
+              exact
+              element={isAuthenticated ? <ProfilePage /> : <SignIn />}
+            />
+            <Route
+              path="/Activities/:id"
+              exact
+              element={isAuthenticated ? <Activities /> : <SignIn />}
+            />
             <Route
               path="/Questions/Questionstab/:id"
               exact
               element={<Questionstab />}
             />
-            <Route path="/UserTags/:id" exact element={<UserTags />} />
+            <Route
+              path="/UserTags/:id"
+              exact
+              element={isAuthenticated ? <UserTags /> : <SignIn />}
+            />
             <Route
               path="/ActivityBadges/:id"
               exact
-              element={<ActivityBadges />}
+              element={isAuthenticated ? <ActivityBadges /> : <SignIn />}
             />
             <Route
               path="/Bookmarkstab/Bookmarkstab/:id"
               exact
               element={<Bookmarkstab />}
             />
-            <Route path="/Reputation/:id" exact element={<Reputation />} />
-            <Route path="/BasicDetails" exact element={<BasicDetails />} />
-            <Route path="/Addtag" exact element={<Addtag />} />
-            <Route path="/Aproove" exact element={<Aproove />} />
-            <Route path="/timeline" exact element={<TimeLine />} />
+            <Route
+              path="/Reputation/:id"
+              exact
+              element={isAuthenticated ? <Reputation /> : <SignIn />}
+            />
+            <Route
+              path="/BasicDetails"
+              exact
+              element={isAuthenticated ? <BasicDetails /> : <SignIn />}
+            />
+            <Route
+              path="/Addtag"
+              exact
+              element={isAuthenticated ? <Addtag /> : <SignIn />}
+            />
+            <Route
+              path="/Aproove"
+              exact
+              element={isAuthenticated ? <Aproove /> : <SignIn />}
+            />
+            <Route
+              path="/timeline"
+              exact
+              element={isAuthenticated ? <TimeLine /> : <SignIn />}
+            />
             <Route
               path="/questionAnalytics"
               exact
-              element={<QuestionAnalytics />}
+              element={isAuthenticated ? <QuestionAnalytics /> : <SignIn />}
             />
             <Route
               path="/topViewedQuestions"
               exact
-              element={<TopQuestions />}
+              element={isAuthenticated ? <TopQuestions /> : <SignIn />}
             ></Route>
 
-            <Route path="/topTags" exact element={<TopTags />}></Route>
+            <Route
+              path="/topTags"
+              exact
+              element={isAuthenticated ? <TopTags /> : <SignIn />}
+            ></Route>
             <Route
               path="/highReputedUsers"
               exact
-              element={<HighReputedUsers />}
+              element={isAuthenticated ? <HighReputedUsers /> : <SignIn />}
             ></Route>
             <Route
               path="/lowReputedUsers"
               exact
-              element={<LowReputedUsers />}
+              element={isAuthenticated ? <LowReputedUsers /> : <SignIn />}
             ></Route>
-            <Route path="/Editdetails/:id" exact element={<EditDetails />} />
+            <Route
+              path="/Editdetails/:id"
+              exact
+              element={isAuthenticated ? <EditDetails /> : <SignIn />}
+            />
           </Routes>
         </div>
       </main>
